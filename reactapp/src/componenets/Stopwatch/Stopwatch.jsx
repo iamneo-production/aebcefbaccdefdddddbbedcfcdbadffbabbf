fc134c9a76212858cf013e53ce224b7710dcf9db
Iamneo <techadmin@iamneo.ai>
2023-08-03 12:55:18 +0000

    Backup Commit

--- reactapp/src/componenets/Stopwatch/Stopwatch.jsx
+++ reactapp/src/componenets/Stopwatch/Stopwatch.jsx
@@ -1,61 +1,58 @@
-import React, { useState } from "react";
-import "./Stopwatch.css";
-import Timer from "../Timer/Timer";
-import ControlButtons from "../ControlButtons/ControlButtons";
-  
-function StopWatch() {
-  const [isActive, setIsActive] = useState(false);
-  const [isPaused, setIsPaused] = useState(true);
-  const [time, setTime] = useState(0);
-  const [resetDisabled, setResetDisabled] = useState(true);
+import { useState } from "react";
 
-  // console.log(resetDisabled)
-  
-  React.useEffect(() => {
-    let interval = null;
-  
-    if (isActive && isPaused === false) {
-      interval = setInterval(() => {
-        setTime((time) => time + 10);
-      }, 10);
-    } else {
-      clearInterval(interval);
-    }
-    return () => {
-      clearInterval(interval);
+const Stopwatch = (props) => {
+    const [time,setTime]=useState({h:0,m:0,s:0});
+    const [status,setStatus]=useState(0);
+    const [interv,setInterv] =useState();
+    const [disabledState,setDisabledState]=useState(true);
+
+    let updatedH = time.h,updatedM=time.m,updatedS=time.s;
+
+    const startHandler = () => {
+        setStatus(1);
+        setDisabledState(false);
+        setInterv(setInterval(run,1000));
     };
-  }, [isActive, isPaused]);
-  
-  const handleStart = () => {
-    setIsActive(true);
-    setResetDisabled(false);
-    setIsPaused(false);
-  };
-  
-  const handlePauseResume = () => {
-    setIsPaused(!isPaused);
-  };
-  
-  const handleReset = () => {
-    setIsActive(false);
-    setResetDisabled(true);
-    setTime(0);
-  };
-  
-  return (
-    <div className="stop-watch">
-      <p className="heading">React StopWatch</p>
-      <Timer time={time} />
-      <ControlButtons
-        active={isActive}
-        isPaused={isPaused}
-        handleStart={handleStart}
-        handlePauseResume={handlePauseResume}
-        handleReset={handleReset}
-        resetDisabled={resetDisabled}
-      />
-    </div>
-  );
-}
-  
-export default StopWatch;
+
+    const run = () => {
+        if(updatedS===60) {
+            updatedM++;
+            updatedS=0;
+        }
+        if(updatedM===60) {
+            updatedH++;
+            updatedM=0;
+        }
+        updatedS++;
+        return setTime({h:updatedH,m:updatedM,s:updatedS});
+    }
+
+    const pauseHandler = () => {
+        clearInterval(interv);
+        setStatus(2);
+    };
+
+    const resetHandler = () => {
+        clearInterval(interv);
+        setStatus(0);
+        setDisabledState(true);
+        setTime({h:0,m:0,s:0});
+    };
+
+
+    return <div>
+        <h1>Stopwatch</h1>
+        <p data-testid="time">{(time.h >= 10) ? time.h : "0"+time.h}:
+        {(time.m >= 10) ? time.m : "0"+time.m}:
+        {(time.s >= 10) ? time.s : "0"+time.s}
+        </p>
+
+        {(status === 0) ? <button name="start" data-testid="start" onClick={startHandler}>Start</button>:''}
+        {(status === 1) ? <button name="pause" data-testid="pause" onClick={pauseHandler}>Pause</button>:''}
+        {(status === 2) ? <button name="resume" data-testid="resume" onClick={startHandler}>Resume</button>:''}
+
+        <button name="reset" data-testid="reset" disabled={disabledState} onClick={resetHandler}>Reset</button>
+    </div>;
+};
+
+export default Stopwatch;
