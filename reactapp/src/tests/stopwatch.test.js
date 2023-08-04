021eae798e94c8ea39f12af356486fa531d0614b
Iamneo <techadmin@iamneo.ai>
2023-08-04 15:02:36 +0000

    Backup Commit

--- reactapp/src/tests/stopwatch.test.js
+++ reactapp/src/tests/stopwatch.test.js
@@ -1,4 +1,4 @@
-import Stopwatch from "../componenets/Stopwatch/Stopwatch"
+import Stopwatch from "../components/Stopwatch/Stopwatch";
 import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 
